e43eea362665e389de1f675ac43f1da06172db72
Sharuk25 <sharukshaik786786@gmail.com>
2026-09-12 16:05:18 +0530

    Deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Adaptive-Layout-Engine-for-Multi-Surface-Ads/',
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
